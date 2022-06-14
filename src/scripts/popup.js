4710a76770d67f398d9059540645404132cfe4f2
Hattan Shobokshi <hattan.shobokshi@microsoft.com>
2022-06-14 05:49:46 +0000

    fixed complete bug

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -36,108 +36,12 @@ function loadData(_client, webContext){
 
       question.innerText = questionOfTheDay.text;
 
-      let notCompletedTeamMembers = [];
-      for(let i=0,len=members.length;i<len;i++){
-        let member = members[i];
-        let found = notCompletedTeamMembers.includes(member.identity.id);
-        
-      }
-      // let notCompletedTeamMembers = members.filter((member) => {
-      //   return !completedTeamMembers.some((teamMember) => {
-      //     return teamMember === member.identity.id;
-      //   });
-      // });
-
-      if (notCompletedTeamMembers.length === 0) {
-        canvas.style.display = "block";
-        const ctx = canvas.getContext("2d");
-        let cx = ctx.canvas.width / 2;
-        let cy = ctx.canvas.height / 2;
-
-        let confetti = [];
-        const confettiCount = 600;
-        const gravity = 0.5;
-        const terminalVelocity = 5;
-        const drag = 0.075;
-        const colors = [
-          { front: 'red', back: 'darkred' },
-          { front: 'green', back: 'darkgreen' },
-          { front: 'blue', back: 'darkblue' },
-          { front: 'yellow', back: 'darkyellow' },
-          { front: 'orange', back: 'darkorange' },
-          { front: 'pink', back: 'darkpink' },
-          { front: 'purple', back: 'darkpurple' },
-          { front: 'turquoise', back: 'darkturquoise' }];
-
-        const initConfetti = () => {
-          for (let i = 0; i < confettiCount; i++) {
-            confetti.push({
-              color: colors[Math.floor(randomRange(0, colors.length))],
-              dimensions: { x: randomRange(10, 20), y: randomRange(10, 30) },
-              position: { x: randomRange(0, canvas.width), y: canvas.height - 1 },
-              rotation: randomRange(0, 2 * Math.PI),
-              scale: { x: 1, y: 1 },
-              velocity: { x: randomRange(-25, 25), y: randomRange(0, -50) }
-            });
-          }
-        };
-
-        const render = () => {
-          ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-          confetti.forEach((confetto, index) => {
-            let width = confetto.dimensions.x * confetto.scale.x;
-            let height = confetto.dimensions.y * confetto.scale.y;
-
-            // Move canvas to position and rotate
-            ctx.translate(confetto.position.x, confetto.position.y);
-            ctx.rotate(confetto.rotation);
-
-            // Apply forces to velocity
-            confetto.velocity.x -= confetto.velocity.x * drag;
-            confetto.velocity.y = Math.min(confetto.velocity.y + gravity, terminalVelocity);
-            confetto.velocity.x += Math.random() > 0.5 ? Math.random() : -Math.random();
-
-            // Set position
-            confetto.position.x += confetto.velocity.x;
-            confetto.position.y += confetto.velocity.y;
-
-            // Delete confetti when out of frame
-            if (confetto.position.y >= canvas.height) confetti.splice(index, 1);
-
-            // Loop confetto x position
-            if (confetto.position.x > canvas.width) confetto.position.x = 0;
-            if (confetto.position.x < 0) confetto.position.x = canvas.width;
-
-            // Spin confetto by scaling y
-            confetto.scale.y = Math.cos(confetto.position.y * 0.1);
-            ctx.fillStyle = confetto.scale.y > 0 ? confetto.color.front : confetto.color.back;
-
-            // Draw confetti
-            ctx.fillRect(-width / 2, -height / 2, width, height);
-
-            // Reset transform matrix
-            ctx.setTransform(1, 0, 0, 1, 0, 0);
-          });
-
-          window.requestAnimationFrame(render);
-        };
-
-        message.innerText = "All team members have completed updating";
-
-        initConfetti();
-        render();
-      }
+      let notCompletedTeamMembers = members;
 
       members.forEach((member) => {
         team.appendChild(createRowForTeamMember(member));
       });
 
-      completedTeamMembers.forEach((member) => {
-        const row = document.getElementById(`member-${member}`);
-        if(row)
-            row.classList.add("completed");
-      });
 
       const startRandomizer = () => {
         let randomizer = 0;
@@ -191,6 +95,9 @@ function loadData(_client, webContext){
       };
 
       buttonStartOver.addEventListener("click", () => {
+        canvas.style.display = "none";
+        message.style.display = "none";
+
         const key = (new Intl.DateTimeFormat().format(new Date())).replace(/\//g, "");
         localStorage.removeItem(key);
 
@@ -218,6 +125,97 @@ function loadData(_client, webContext){
         });
 
         startRandomizer();
+
+        let completed = document.getElementsByClassName('completed');
+        let green = document.getElementsByClassName('green');
+        let finished = completed.length + green.length;
+        console.log("finished=" + finished);
+
+        let limit = (members.length-1);
+        console.log("limit=" + limit)
+        if (limit === 0) {
+          canvas.style.display = "block";
+          message.style.display = "block";
+
+          const ctx = canvas.getContext("2d");
+          let cx = ctx.canvas.width / 2;
+          let cy = ctx.canvas.height / 2;
+  
+          let confetti = [];
+          const confettiCount = 600;
+          const gravity = 0.5;
+          const terminalVelocity = 5;
+          const drag = 0.075;
+          const colors = [
+            { front: 'red', back: 'darkred' },
+            { front: 'green', back: 'darkgreen' },
+            { front: 'blue', back: 'darkblue' },
+            { front: 'yellow', back: 'darkyellow' },
+            { front: 'orange', back: 'darkorange' },
+            { front: 'pink', back: 'darkpink' },
+            { front: 'purple', back: 'darkpurple' },
+            { front: 'turquoise', back: 'darkturquoise' }];
+  
+          const initConfetti = () => {
+            for (let i = 0; i < confettiCount; i++) {
+              confetti.push({
+                color: colors[Math.floor(randomRange(0, colors.length))],
+                dimensions: { x: randomRange(10, 20), y: randomRange(10, 30) },
+                position: { x: randomRange(0, canvas.width), y: canvas.height - 1 },
+                rotation: randomRange(0, 2 * Math.PI),
+                scale: { x: 1, y: 1 },
+                velocity: { x: randomRange(-25, 25), y: randomRange(0, -50) }
+              });
+            }
+          };
+  
+          const render = () => {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+  
+            confetti.forEach((confetto, index) => {
+              let width = confetto.dimensions.x * confetto.scale.x;
+              let height = confetto.dimensions.y * confetto.scale.y;
+  
+              // Move canvas to position and rotate
+              ctx.translate(confetto.position.x, confetto.position.y);
+              ctx.rotate(confetto.rotation);
+  
+              // Apply forces to velocity
+              confetto.velocity.x -= confetto.velocity.x * drag;
+              confetto.velocity.y = Math.min(confetto.velocity.y + gravity, terminalVelocity);
+              confetto.velocity.x += Math.random() > 0.5 ? Math.random() : -Math.random();
+  
+              // Set position
+              confetto.position.x += confetto.velocity.x;
+              confetto.position.y += confetto.velocity.y;
+  
+              // Delete confetti when out of frame
+              if (confetto.position.y >= canvas.height) confetti.splice(index, 1);
+  
+              // Loop confetto x position
+              if (confetto.position.x > canvas.width) confetto.position.x = 0;
+              if (confetto.position.x < 0) confetto.position.x = canvas.width;
+  
+              // Spin confetto by scaling y
+              confetto.scale.y = Math.cos(confetto.position.y * 0.1);
+              ctx.fillStyle = confetto.scale.y > 0 ? confetto.color.front : confetto.color.back;
+  
+              // Draw confetti
+              ctx.fillRect(-width / 2, -height / 2, width, height);
+  
+              // Reset transform matrix
+              ctx.setTransform(1, 0, 0, 1, 0, 0);
+            });
+
+            window.requestAnimationFrame(render);
+          };
+          
+          message.innerText = "All team members have completed updating";
+  
+          initConfetti();
+          render();
+        }
+        
       });
 
       //buttonNext.click();
